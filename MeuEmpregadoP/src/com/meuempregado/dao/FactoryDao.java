e728b7a6850f265eeb1ca0b6f74b6203dbbc941a
Daniel <Daniel@10.20.2.206>
2017-11-13 19:43:57 -0200

    CS09 - Contrato de Seguro de Vida

--- MeuEmpregadoP/src/com/meuempregado/dao/FactoryDao.java
+++
@@ -1,8 +0,0 @@
-package com.meuempregado.dao;
-
-public class FactoryDao {
-		
-	public static <E> InterfaceGenericDao<E> createGenericDao(Class<E> entityClass){
-		return new GenericDao<E>(entityClass);
-	}
-}
